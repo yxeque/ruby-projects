29d10481740f87e75a93b38b1fa5a995b1a68dc1
yxeque <yxequel@gmail.com>
2024-01-26 18:27:08 +0800

    Fix inconsistencies with character cases

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,4 +1,4 @@
-def caesar_cipher(string, shift_factor)
+def caesar_cipher(string, shift_factor = 3)
   alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
   shift_factor = shift_factor.to_i
   shifted_alphabet = alphabet[shift_factor..-1] + alphabet[0...shift_factor]
@@ -7,9 +7,17 @@ def caesar_cipher(string, shift_factor)
 
   string.chars.each do |char|
     if char.match?(/[a-zA-Z]/)
-      index = alphabet.index(char)
+      original_case = char.upcase == char  # Store original case (upper or lower)
+      index = alphabet.index(char.downcase)  # Find index based on lowercase version
       new_char = shifted_alphabet[index]
-      result += new_char  
+
+      # Maintain original case using modulo operation
+      new_char = (new_char.ord + 26 - 'a'.ord) % 26 + 'a'.ord if original_case
+      new_char = new_char.chr
+      new_char = new_char.upcase if original_case
+      new_char = new_char.downcase unless original_case
+
+      result += new_char
     else
       result += char  
     end
@@ -18,5 +26,7 @@ def caesar_cipher(string, shift_factor)
   result  
 end
 
-puts caesar_cipher("Hello World!", 4)  
+puts caesar_cipher("Hello World!", 4)
+puts caesar_cipher "Hey"
+puts caesar_cipher "I love zebras", 2
 
